9733234a838914d967331e9aa1ede01b22b66d50
Fuzail Ahamad <fuzailahamad2002@gmail.com>
2026-07-09 12:41:29 +0530

    Side bar changed

--- frontend/src/components/Layout/Sidebar.jsx
+++ frontend/src/components/Layout/Sidebar.jsx
@@ -1,17 +1,26 @@
-import { Bot } from "lucide-react";
+import { Bot, Plus } from "lucide-react";
 
 function Sidebar() {
   return (
-    <div className="w-64 h-screen bg-zinc-900 text-white">
+    <div className="w-64 h-screen bg-zinc-900 text-white flex flex-col">
 
+      {/* Sidebar Header */}
       <div className="flex items-center gap-3 p-5 border-b border-zinc-800">
-
-        <Bot size={28} />
-
+        <Bot size={40} />
         <h1 className="text-xl font-bold">
           Personal Assistant
         </h1>
+      </div>
 
+      {/* New Chat Button */}
+      <div className="p-4">
+        <button className="w-full flex items-center justify-center gap-2 bg-zinc-800 hover:bg-zinc-700 transition rounded-lg py-3 cursor-pointer">
+
+          <Plus size={20} />
+
+          <span>New Chat</span>
+
+        </button>
       </div>
 
     </div>
